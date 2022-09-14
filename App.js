6cc605121a53c993b7a3a4cdaf0e401545d5dcac
Milan Jovanovic <mjovanovic@twelfthman.co>
2022-09-14 12:32:22 +0200

    finish todo app

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import TaskInput from "./components/TaskInput";
 import TaskItem from "./components/TaskItem";
 
 export default function App() {
   const [taskList, setTaskList] = useState([]);
+  const [modalIsVisable, setModalIsVisable] = useState(false);
 
   const handleAddTask = (taskText) => {
     setTaskList((prevState) => [
@@ -25,24 +27,38 @@ export default function App() {
     setTaskList((prevState) => prevState.filter((item, idx) => idx != index));
   };
 
+  const handleModalIsVisable = () => {
+    setModalIsVisable(!modalIsVisable);
+  };
+
   return (
-    <View style={styles.appContainer}>
-      <TaskInput handleAddTask={handleAddTask} />
-      <View style={styles.taskContainer}>
-        <FlatList
-          data={taskList}
-          alwaysBounceVertical={false}
-          renderItem={(itemData) => (
-            <TaskItem
-              itemData={itemData}
-              handleRemoveTask={handleRemoveTask}
-              handleCompleteTask={handleCompleteTask}
-            />
-          )}
-          keyExtractor={(item, index) => item.id}
-        />
+    <>
+      <StatusBar style="light"/>
+      <View style={styles.appContainer}>
+        <Button title="Add new Task" onPress={handleModalIsVisable} />
+        {modalIsVisable && (
+          <TaskInput
+            handleAddTask={handleAddTask}
+            visible={modalIsVisable}
+            handleModalIsVisable={handleModalIsVisable}
+          />
+        )}
+        <View style={styles.taskContainer}>
+          <FlatList
+            data={taskList}
+            alwaysBounceVertical={false}
+            renderItem={(itemData) => (
+              <TaskItem
+                itemData={itemData}
+                handleRemoveTask={handleRemoveTask}
+                handleCompleteTask={handleCompleteTask}
+              />
+            )}
+            keyExtractor={(item, index) => item.id}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
--- components/TaskInput.js
+++ components/TaskInput.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { Button, StyleSheet, TextInput, View } from "react-native";
+import {
+  Button,
+  Modal,
+  StyleSheet,
+  TextInput,
+  View,
+  Image,
+} from "react-native";
+import task from "../assets/images/task.png";
 
 const TaskInput = (props) => {
   const [taskText, setTaskText] = useState("");
@@ -11,41 +19,64 @@ const TaskInput = (props) => {
   const handleAddTask = () => {
     props.handleAddTask(taskText);
     setTaskText("");
+    props.handleModalIsVisable();
   };
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Discribe your task..."
-        style={styles.inputField}
-        onChangeText={handleTaskInput}
-        value={taskText}
-      />
-      <Button
-        title="Add Task"
-        disabled={taskText.length === 0}
-        onPress={handleAddTask}
-      />
-    </View>
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <Image style={styles.image} source={task} />
+        <TextInput
+          placeholder="Discribe your task..."
+          style={styles.inputField}
+          onChangeText={handleTaskInput}
+          value={taskText}
+        />
+        <View style={styles.buttonsContainer}>
+          <View>
+            <Button
+              title="Add Task"
+              disabled={taskText.length === 0}
+              onPress={handleAddTask}
+            />
+          </View>
+          <View>
+            <Button title="Cancel" onPress={props.handleModalIsVisable} />
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
     borderBottomColor: "gray",
+    backgroundColor: "#311b6b",
   },
   inputField: {
+    backgroundColor: "white",
+    borderRadius: 6,
+    width: "90%",
     borderWidth: 1,
     borderColor: "blue",
-    flex: 1,
-    padding: 10,
-    marginRight: 8,
+    padding: 16,
+    color: "white",
+  },
+  buttonsContainer: {
+    width: "90%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
 });
 
